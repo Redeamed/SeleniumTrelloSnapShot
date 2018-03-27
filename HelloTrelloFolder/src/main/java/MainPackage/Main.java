538a9c191f67e15105f76fc055c9e51c0a9c297d
Kyle Davis <kdavis928@yahoo.com>
2018-03-26 21:14:51 -0400

    Cleaning up the code
    Added comments, broke selenium class into methods.  Seperated Swing UI
    to methods.

--- HelloTrelloFolder/src/main/java/MainPackage/Main.java
+++ HelloTrelloFolder/src/main/java/MainPackage/Main.java
@@ -2,45 +2,53 @@ package MainPackage;
 
 import java.io.IOException;
 
+import org.openqa.selenium.By;
+
 import SeleniumPackage.SeleniumMain;
 
 public class Main 
 {
-
+	static SwingUI mainUI;
+	static SeleniumMain seleniumMain;
+	
 	public static void main(String[] args) 
 	{
+
+		mainUI = new SwingUI("Trello SnapShot", 400,300,true);
 		
+	}
+	
+	public static void InitializeSelenium(String login, String password, String boardName)
+	{
+		seleniumMain = new SeleniumMain();
+		seleniumMain.OpenWebPage("https://trello.com/");
 		
+		//click login in
+		seleniumMain.ClickXpathElement("/html/body/div[1]/div[2]/a[1]"); 
+		//input login information
+        seleniumMain.SendStringToXpathElement("//*[@id=\"user\"]", login);
+        //input password information
+        seleniumMain.SendStringToXpathElement("//*[@id=\"password\"]", password);
+        //click submit login information
+		seleniumMain.ClickXpathElement("//*[@id=\"login\"]"); 
+        
 		
-		SwingUI mainUI = new SwingUI("Trello SnapShot", 400,300,true);
-		
-		
-		String baseUrl = "http://Trello.com";
-		//SeleniumMain sM = new SeleniumMain();
-		//sM.SetDriverFF();
-		//sM.OpenURL(baseUrl);
-		//sM.CloseDriver();
-		
-		//ExcelRW excelWR = new ExcelRW();
-		//excelWR.LoadfileToPrimary("C:\\Users\\Kyle\\WorkspaceGit\\project\\HelloTrelloFolder\\SaveFolder\\SaveTest.xml");
-		/*
-		for(int r = 0; r < 10; r++)
-		{
-			for(int c = 0; c < 10; c++)
-			{
-				System.out.println("\n Row: "+ r + " Col: " + c);
-				excelWR.WriteToCell("test(" + r +","+c +")", r, c);
-				
-			}
-		}
-		
-		
-		try {
-			excelWR.SaveWorkbook("C:\\Users\\Kyle\\WorkspaceGit\\project\\HelloTrelloFolder\\SaveFolder\\SaveTest.xml");
-		} catch (IOException e) {
+		 
+        try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e1) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			e1.printStackTrace();
 		}
-		*/
+        seleniumMain.OpenTargetBoard(boardName);
+        
+        try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+        
+        seleniumMain.Close();
 	}
 }
